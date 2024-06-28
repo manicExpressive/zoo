b809b66aa922f04cf5edd6d529bf99a1cd7c44de
Joshua Blount <hi@joshuablount.com>
2024-06-28 08:59:54 -0700

    Remove luxon, we're doing less!

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,4 @@
-const { DateTime } = require("luxon");
-
-/**
-* This is the JavaScript code that determines the config for your Eleventy site
-*
-* You can add lost of customization here to define how the site builds your content
-* Try extending it to suit your needs!
-*/
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats([
-    // Templates:
-    "html",
-    "njk",
-    "md",
-    // Static Assets:
-    "css",
-    "jpeg",
-    "jpg",
-    "png",
-    "svg",
-    "woff",
-    "woff2"
-  ]);
-
   eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
 
   return {
